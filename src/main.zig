f97dd0ffc28459e4aa737108f53177a7a0fa33d0
Daniel Kurz <danielkurz03@protonmail.com>
2022-06-07 00:37:27 +0200

    get sha1 hash working

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,30 @@ const std = @import("std");
 const allocator = std.heap.page_allocator;
 const ArrayList = std.ArrayList;
 
-pub fn encode() anyerror!ArrayList(u32) {
+pub fn encode(_: []const u32) anyerror!ArrayList(u32) {
     var result = ArrayList(u32).init(allocator);
+
     return result;
 }
 
 pub fn main() anyerror!void {
-    var list = try encode();
-    defer list.deinit();
-
-    try list.append(1);
-    try list.append(2);
-    try list.append(3);
-
-    std.log.info("the list has {d} items", .{list.items.len});
-    for (list.items) |item| {
-        std.log.info("i: {d}", .{item});
-    }
+    var hash: [20]u8 = undefined;
+    std.crypto.hash.Sha1.hash("our data to hash", &hash, .{});
+    std.log.info("our hash is: {s}", .{hash});
 }
+
+// pub fn main() anyerror!void {
+//     var data = [_]u32{1, 2, 3};
+
+//     var list = try encode(&data);
+//     defer list.deinit();
+
+//     try list.append(1);
+//     try list.append(2);
+//     try list.append(3);
+
+//     std.log.info("the list has {d} items", .{list.items.len});
+//     for (list.items) |item| {
+//         std.log.info("i: {d}", .{item});
+//     }
+// }
